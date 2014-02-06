d4a1aacd380b57dcba912e3b7552110697447188
ryanorsinger <ryan.orsinger@gmail.com>
2014-02-06 15:56:33 -0600

    1st exercise of Do-While in Control Structures I section 5 of 5

---
+++ php/do_while.php
@@ -0,0 +1,21 @@
+<?php
+
+// Create a do-while loop that will count by 2's and will
+// return the result $a * $a on each line 
+// while $a is less than 1,000,000. 
+// output should equal
+// 2
+// 4
+// 16
+// 256
+// 65536
+// first commit will be for this exercise
+
+$a = 2;
+
+do {
+	echo "\$a is equal to {$a}\n";
+	$a = $a * $a;
+} while ($a < 1000000);
+
+?>
