310d9e50dd30eca0b066725154247c5e151edaf9
ryanorsinger <ryan.orsinger@gmail.com>
2014-02-10 10:57:53 -0600

    exercise 1 with foreach

--- php/foreach.php
+++ php/foreach.php
@@ -1,9 +1,36 @@
 <?php
 
-$data = array('apple', 'banana', 5, 7, 88.1, FALSE);
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach($things as $item) {
+	
+	if (is_int($item)) {
+		echo "{$item} is an integer.\n";
+	
+	} elseif (is_float($item)) {
+		echo "{$item} is a float.\n";
+	
+	} elseif (is_bool($item)) {
+		echo "{$item} is a boolean.\n";
+	
+	} elseif (is_array($item)) {
+		echo "{$item} is an array.\n";
+	
+	} elseif (is_null($item)) {
+		echo "{item} is a null.\n";
+	
+	} elseif (is_string($item)) {
+		echo "{$item} is a string.\n";
+	}
+}
 
 
-foreach ($data as $item) {
+// $animal_types = array('dogs', 'cats', 'birds', 'narwhals');
+// foreach ($animal_types as $animal) {
+//     echo "Old McDonald had a farm, and on that farm he raised some {$animal}\n";
+// }
+
+/*foreach ($data as $item) {
 	if (is_numeric($item)) {
 		echo "{$item} is a number \n";
 	} elseif (is_string($item)) {
@@ -11,7 +38,7 @@ foreach ($data as $item) {
 	} else {
 		echo "{$item} is not a string or a number\n";
 	}
-}
+}*/
 
 
 ?>
