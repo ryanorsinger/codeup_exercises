b24871e0b7f3bce6a9583b91e7aeb32d2c1254df
vagrant <vagrant@quantal.codeup.dev>
2014-02-11 11:43:59 -0600

    second commit - created, added, and tested a modulus function

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -2,7 +2,7 @@
 //arithmetic
 
 $a = 10;
-$b = 2;
+$b = 2.23;
 
 function add($a, $b) {
 	echo $a + $b . PHP_EOL;
@@ -20,20 +20,15 @@ function divide($a, $b) {
 	echo $a / $b . PHP_EOL;
 }
 
+function modulus($a, $b) {
+	echo ($a%$b) . PHP_EOL;
+}
+
 add($a, $b);
 subtract($a, $b);
 multiply($a, $b);
 divide($a, $b);
 
-// but notice that this doens't work quite as I intended
-echo "\t and here are the values passed into the functions backwards" . PHP_EOL;
-echo "\t But notice that this is not quite what I planned to have." . PHP_EOL;
-echo  "\t That's because the functions (add, subtract, multiply, divide) right now have a set order of operations" . PHP_EOL;
-
-add($b, $a);
-subtract($b, $a);
-multiply($b, $a);
-divide($b, $a);
-
+modulus($a, $b);
 
 ?>
