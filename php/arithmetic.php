d1534c712cac07fb324a96408d174a30027fe97f
vagrant <vagrant@quantal.codeup.dev>
2014-02-11 13:29:46 -0600

    version with if (is_numeric(var)) on each line

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,34 +1,74 @@
 <?php
-//arithmetic
+//arithmetic.php
 
-$a = 10;
-$b = 2.23;
+// fwrite(STDOUT, "Please input the first number." . PHP_EOL);
+
+// Promp the user for two arguments, their two numbers.
+
+fwrite(STDOUT, "Input your first number." . PHP_EOL);
+$num1 = fgets (STDIN);
+
+// Prompt the user for their second number
+fwrite(STDOUT, "Input your second number." . PHP_EOL);
+$num2 = fgets (STDIN);
+
+$a = $num1;
+$b = $num2;
 
 function add($a, $b) {
-	echo $a + $b . PHP_EOL;
+		if ((is_numeric($a)) && (is_numeric($b))) {
+			echo $a + $b . PHP_EOL;
+			} else { 
+				echo "ERROR: Both arguments must be numbers!" . "PHP_EOL";
+
+		}
 }
+
 
 function subtract($a, $b) {
-	echo $a - $b . PHP_EOL;
+		if ((is_numeric($a)) && (is_numeric($b))) {
+			echo $a - $b . PHP_EOL;
+			} else { 
+				echo "ERROR: Both arguments must be numbers!" . "PHP_EOL";
+
+		}
 }
+
 
 function multiply($a, $b) {
-	echo $a * $b . PHP_EOL;
+		if ((is_numeric($a)) && (is_numeric($b))) {
+			echo $a * $b . PHP_EOL;
+			} else { 
+				echo "ERROR: Both arguments must be numbers!" . "PHP_EOL";
+
+		}
 }
+
 
 function divide($a, $b) {
-	echo $a / $b . PHP_EOL;
+		if ((is_numeric($a)) && (is_numeric($b))) {
+			echo $a / $b . PHP_EOL;
+			} else { 
+				echo "ERROR: Both arguments must be numbers!" . "PHP_EOL";
+
+		}
 }
 
+
 function modulus($a, $b) {
-	echo ($a%$b) . PHP_EOL;
+		if ((is_numeric($a)) && (is_numeric($b))) {
+			echo $a % $b . PHP_EOL;
+			} else { 
+				echo "ERROR: Both arguments must be numbers!" . "PHP_EOL";
+
+		}
 }
+
 
 add($a, $b);
 subtract($a, $b);
 multiply($a, $b);
 divide($a, $b);
-
 modulus($a, $b);
 
 ?>
