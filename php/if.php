327f418f47b188e864e2ab939cc511d9abc3d946
vagrant <vagrant@quantal.codeup.dev>
2014-02-17 18:10:03 -0600

    version 2 with if else

--- php/if.php
+++ php/if.php
@@ -6,46 +6,49 @@
 
 $a = 5;
 $b = 10;
-$c = '10';
+$c = 10;
+
+if ($a == $b) {
+	echo "$a is equal to $b\n";
+} else {
+	echo "$a is not equal to $b\n";
+	}
 
 if ($a < $b) {
     // output the appropriate result
     echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
-    // output the appropriate result
-    echo "$b is greater than $a\n";
-}
+} else {
+	echo "$a is greater than $b\n";
+	}
 
 if ($b >= $c) {
     // output the appropriate result
     echo "$b is greater than or equal to $c\n";
+} else {
+	echo "$b is not greater or equal to $c\n";
 }
 
 if ($b <= $c) {
-    // output the appropriate result
     echo "$b is less than or equal to $c\n";
+} else {
+	echo "b is not less than or equal to $c\n";
 }
 
 if ($b == $c) {
     // output the appropriate result
     echo "$b is equal to $c\n";
+} else {
+	echo "$b is not equal to $c\n";
 }
 
 if ($b === $c) {
     // output the appropriate result
     echo "$b is identical to $c\n";
-}
+} else {
+	echo "$b is not identical to $c\n";
+	}
 
-// output the appropriate result
-if ($b != $c) {
-    echo "$b is not equal to $c\n";
-}
 
-// output the appropriate result
-if ($b !== $c) {
-    echo ($b . " is not identical to " . $c) . PHP_EOL;
-}
+
 
 ?> 
