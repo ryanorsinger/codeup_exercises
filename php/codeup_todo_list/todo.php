76d5a81e14915b31022330db896939317e1d82dc
ryanorsinger <ryan.orsinger@gmail.com>
2014-02-20 17:49:12 -0600

    more functionality

--- php/codeup_todo_list/todo.php
+++ php/codeup_todo_list/todo.php
@@ -3,44 +3,102 @@
 // Create array to hold list of todo items
 $items = array();
 
+// Iterate through list items
 function list_items($items) {
-
+    foreach ($items as $key => $item) {
+        // Display each item and key +1
+        $j = $key + 1;
+        echo "[{$j}] {$item}\n";
+    }
 }
 
-function get_input($upper == FALSE) {
 
-    
+function get_input($upper = FALSE) {
+    if($upper == TRUE) {
+        $upper = trim(fgets(STDIN)); 
+        return strtoupper($upper);
+    } else {
+        $upper = trim(fgets(STDIN));
+        return $upper;
+    }
 }
+
+function sort_menu($items) {
+    echo "Would you like to sort your todo list items (A)-Z, (Z)-A, (O)rder entered, (R)everse order entered\n";
+    $order = get_input(TRUE);
+
+    switch($order) {
+        case 'A':
+            asort($items);
+            break;
+        case 'Z':
+            arsort($items);
+            break;
+        case 'O':
+            ksort($items);
+            break;
+        case 'R':
+            krsort($items);
+            break;
+    }
+    return $items;
+}
+
 
 // The loop!
 do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
+    // Echo the list produced by the function
+    echo list_items($items);
 
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, remove (F)irst item, remove (L)ast item, (R)emove item, (SO)rt, (Q)uit : ';
+    // Get the input from the
+    $input = get_input(TRUE);
 
-    // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
-
+switch ($input) {
     // Check for actionable input
-    if ($input == 'N') {
+    case 'N': 
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R') {
+        $item_input = get_input();
+
+        echo "Add to (B)eginning or (E)nd of list?";
+        $order = get_input(TRUE);
+
+        if ($order == 'B') {
+            array_unshift($items, $item_input);
+        } else {
+            $items[] = $item_input;
+        }
+        break;
+
+    case 'R':
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
         $key = trim(fgets(STDIN));
         // Remove from array
         unset($items[$key]);
+        break;
+    
+    case 'F':
+        // remove the first item from the list
+        array_shift($items);
+        break;
+    
+    case 'L':
+        // remove the last item from the list
+        array_pop($items);
+        break;
+
+    case 'SO':
+        // When sort menu is opened, show the following options "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered".
+        $items = sort_menu($items);
+        break;
     }
+
+
 // Exit when input is (Q)uit
 } while ($input != 'Q');
 
@@ -49,3 +107,5 @@ echo "Goodbye!\n";
 
 // Exit with 0 errors
 exit(0);
+
+?>
