fb8ffcd27f96da0684cf98adca1d25dd69ab03d3
vagrant <vagrant@quantal.codeup.dev>
2014-03-13 13:55:57 -0500

    MySQL connection from PHP using dummy database

---
+++ php/php_mysqli.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// Get new instance of MySQLi object
+$mysqli = @new mysqli('127.0.0.1', 'codeup', 'password', 'codeup_mysqli_test_db');
+
+// Check for errors
+if ($mysqli->connect_errno) {
+    echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+
+?>
